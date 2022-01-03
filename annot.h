7e4110c4bac459de61c4b9e730673767ef8cea93
talbii <ido@tal.bi>
2022-01-03 22:24:11 +0200

    Added annotations header

---
+++ annot.h
@@ -0,0 +1,14 @@
+/*
+ * annot.h -- macro.h Annotations
+ * */
+
+#ifndef TMACRO_H_ANNOT
+#define TMACRO_H_ANNOT
+
+#define NotImplemented
+#define Deprecated
+#define NotNull
+#define UnitTest
+#define Test
+
+#endif /*TMACRO_H_ANNOT*/

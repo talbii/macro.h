22b5540bae987bb685d18153ee014adc128a5767
talbii <ido@tal.bi>
2022-01-12 23:19:23 +0200

    Added token pasting macro

---
+++ _tmacro_paste_token.h
@@ -0,0 +1,5 @@
+#ifndef TMACROi_H_PASTE_TOKEN
+#define TMACROi_H_PASTE_TOKEN
+#define tmacro__paste_tokens(a, b) a ## b
+#define PASTE_TOKEN(a, b) tmacro__paste_tokens(a, b)
+#endif /*TMACROi_H_PASTE_TOKEN*/
